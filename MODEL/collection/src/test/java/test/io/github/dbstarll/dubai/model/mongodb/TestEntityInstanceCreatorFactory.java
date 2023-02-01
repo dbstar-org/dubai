742701b494734ce14204cfa60f1dbd558751be53
dbstarll <dbstarll@gmail.com>
2023-02-01 19:44:42 +0800

    去掉Mock的使用

--- MODEL/collection/src/test/java/test/io/github/dbstarll/dubai/model/mongodb/TestEntityInstanceCreatorFactory.java
+++ MODEL/collection/src/test/java/test/io/github/dbstarll/dubai/model/mongodb/TestEntityInstanceCreatorFactory.java
@@ -2,14 +2,15 @@ package test.io.github.dbstarll.dubai.model.mongodb;
 
 import io.github.dbstarll.dubai.model.collection.test.SimpleGenericEntity;
 import io.github.dbstarll.dubai.model.mongodb.EntityInstanceCreatorFactory;
-import mockit.Expectations;
-import mockit.Mocked;
+import org.bson.codecs.pojo.ClassModel;
+import org.bson.codecs.pojo.EntityConvention;
 import org.bson.codecs.pojo.InstanceCreator;
 import org.bson.codecs.pojo.InstanceCreatorFactory;
-import org.bson.codecs.pojo.PropertyAccessor;
 import org.bson.codecs.pojo.PropertyModel;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Arrays;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -18,9 +19,6 @@ import static org.junit.Assert.assertSame;
 
 public class TestEntityInstanceCreatorFactory {
     private InstanceCreatorFactory<SimpleGenericEntity> instanceCreatorFactory;
-
-    @Mocked
-    PropertyModel<Integer> propertyModel;
 
     @Before
     public void initialize() {
@@ -52,22 +50,10 @@ public class TestEntityInstanceCreatorFactory {
         final InstanceCreator<SimpleGenericEntity> instanceCreator = instanceCreatorFactory.create();
         final Integer value = 100;
 
-        new Expectations() {
-            {
-                propertyModel.getPropertyAccessor();
-                result = new PropertyAccessor<Integer>() {
-                    @Override
-                    public <S> Integer get(S instance) {
-                        return null;
-                    }
-
-                    @Override
-                    public <S> void set(S instance, Integer value) {
-                        ((SimpleGenericEntity) instance).setValue(value);
-                    }
-                };
-            }
-        };
+        final PropertyModel propertyModel = ClassModel.builder(SimpleGenericEntity.class)
+                .conventions(Arrays.asList(new EntityConvention()))
+                .build()
+                .getPropertyModel("value");
 
         instanceCreator.set(value, propertyModel);
         final SimpleGenericEntity entity = instanceCreator.getInstance();
