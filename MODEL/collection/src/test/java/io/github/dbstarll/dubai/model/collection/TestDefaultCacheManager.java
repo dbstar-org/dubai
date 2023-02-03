ae5eaa2482ba3050d2cec54ad073098194292ade
dbstarll <dbstarll@gmail.com>
2023-02-03 19:17:17 +0800

    去掉Mocked，改为使用嵌入式数据库进行测试

--- MODEL/collection/src/test/java/io/github/dbstarll/dubai/model/collection/TestDefaultCacheManager.java
+++ MODEL/collection/src/test/java/io/github/dbstarll/dubai/model/collection/TestDefaultCacheManager.java
@@ -1,10 +1,10 @@
 package io.github.dbstarll.dubai.model.collection;
 
+import io.github.dbstarll.dubai.model.MongodTestCase;
 import io.github.dbstarll.dubai.model.cache.EntityCacheManager;
 import io.github.dbstarll.dubai.model.collection.test.SimpleEntity;
 import io.github.dbstarll.dubai.model.entity.EntityFactory;
 import io.github.dbstarll.dubai.model.notify.NotifyType;
-import mockit.Injectable;
 import org.bson.types.ObjectId;
 import org.junit.Test;
 
@@ -12,27 +12,28 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 import static org.junit.Assert.assertTrue;
 
-public class TestDefaultCacheManager {
-    @Injectable
-    Collection<SimpleEntity> collection;
+public class TestDefaultCacheManager extends MongodTestCase {
+    private final Class<SimpleEntity> entityClass = SimpleEntity.class;
 
     @Test
     public void test() {
-        final EntityCacheManager entityCacheManager = new CacheableCollection<SimpleEntity>(collection) {
-            public EntityCacheManager cacheManager() {
-                return getEntityCacheManager();
-            }
-        }.cacheManager();
+        useCollection(entityClass, c -> {
+            final EntityCacheManager entityCacheManager = new CacheableCollection<SimpleEntity>(c) {
+                public EntityCacheManager cacheManager() {
+                    return getEntityCacheManager();
+                }
+            }.cacheManager();
 
-        final AtomicBoolean call = new AtomicBoolean(false);
-        entityCacheManager.find(SimpleEntity.class, new ObjectId().toHexString(),
-                key -> {
-                    call.set(true);
-                    return null;
-                });
-        assertTrue(call.get());
+            final AtomicBoolean call = new AtomicBoolean(false);
+            entityCacheManager.find(SimpleEntity.class, new ObjectId().toHexString(),
+                    key -> {
+                        call.set(true);
+                        return null;
+                    });
+            assertTrue(call.get());
 
-        entityCacheManager.update(SimpleEntity.class, new ObjectId(), NotifyType.INSERT);
-        entityCacheManager.set("cacheKey", EntityFactory.newInstance(SimpleEntity.class));
+            entityCacheManager.update(SimpleEntity.class, new ObjectId(), NotifyType.INSERT);
+            entityCacheManager.set("cacheKey", EntityFactory.newInstance(SimpleEntity.class));
+        });
     }
 }
