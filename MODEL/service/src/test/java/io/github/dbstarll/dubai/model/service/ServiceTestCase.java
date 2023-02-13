aa41190e50a808c38086a27194b8f3207a2c8b76
dbstarll <dbstarll@gmail.com>
2023-02-13 15:09:04 +0800

    注入ImplementalAutowirer

--- MODEL/service/src/test/java/io/github/dbstarll/dubai/model/service/ServiceTestCase.java
+++ MODEL/service/src/test/java/io/github/dbstarll/dubai/model/service/ServiceTestCase.java
@@ -8,9 +8,20 @@ import java.util.function.Consumer;
 public abstract class ServiceTestCase extends MongodTestCase {
     protected final <E extends Entity, S extends Service<E>> void useService(final Class<S> serviceClass,
                                                                              final Consumer<S> consumer) {
+        useService(serviceClass, null, consumer);
+    }
+
+    protected final <E extends Entity, S extends Service<E>> void useService(
+            final Class<S> serviceClass, final ImplementalAutowirer implementalAutowirer, final Consumer<S> consumer) {
         final Class<E> entityClass = ServiceFactory.getEntityClass(serviceClass);
         if (entityClass != null) {
-            useCollection(entityClass, collection -> consumer.accept(ServiceFactory.newInstance(serviceClass, collection)));
+            useCollection(entityClass, collection -> {
+                final S service = ServiceFactory.newInstance(serviceClass, collection);
+                if (service instanceof ImplementalAutowirerAware) {
+                    ((ImplementalAutowirerAware) service).setImplementalAutowirer(implementalAutowirer);
+                }
+                consumer.accept(service);
+            });
         }
     }
 }
